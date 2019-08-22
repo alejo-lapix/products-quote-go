9e8ff528028a77470bf19322269eeac0fa1b93f0
Alejandro Rivera <seeealejandro@gmail.com>
2019-08-22 17:25:08 -0500

    [Progress] Localization module added
    - Countries, Zones, repositories, locationRepository

--- pkg/locations/locations.go
+++ pkg/locations/locations.go
@@ -1,6 +1,8 @@
 package locations
 
-import "products-quote-go/pkg/responsibles"
+import (
+	"products-quote-go/pkg/responsibles"
+)
 
 type Country struct {
 	ID   *string
@@ -8,21 +10,15 @@ type Country struct {
 }
 
 type Zone struct {
-	ID        *string
-	Name      *string
-	CountryId *string
-	SellersID []*string
-	sellers   []*responsibles.User
+	ID      *string
+	Name    *string
+	Country *Country
+	Sellers []*responsibles.User
 }
 
-func (zone *Zone) Sellers() ([]*responsibles.User, error) {
-	return zone.sellers, nil
-}
-
-type ZonesByProduct struct {
-	ID        *string
+type ZonesByProductID struct {
 	ProductID *string
-	ZonesID   []*string
+	Zones     []*Zone
 }
 
 type CountryRepository interface {
@@ -37,4 +33,41 @@ type ZoneRepository interface {
 	Remove(ID *string) error
 	Store(*Zone) error
 	Update(ID *string, zone *Zone) error
+}
+
+type LocationRepository struct {
+	CountryRepository CountryRepository
+	ZoneRepository    ZoneRepository
+}
+
+func (repository *LocationRepository) FindCountry(ID *string) (*Country, error) {
+	return repository.CountryRepository.Find(ID)
+}
+
+func (repository *LocationRepository) RemoveCountry(ID *string) error {
+	return repository.CountryRepository.Remove(ID)
+}
+
+func (repository *LocationRepository) StoreCountry(country *Country) error {
+	return repository.CountryRepository.Store(country)
+}
+
+func (repository *LocationRepository) UpdateCountry(ID *string, country *Country) error {
+	return repository.CountryRepository.Update(ID, country)
+}
+
+func (repository *LocationRepository) FindZone(ID *string) (*Zone, error) {
+	return repository.ZoneRepository.Find(ID)
+}
+
+func (repository *LocationRepository) RemoveZone(ID *string) error {
+	return repository.ZoneRepository.Remove(ID)
+}
+
+func (repository *LocationRepository) StoreZone(zone *Zone) error {
+	return repository.ZoneRepository.Store(zone)
+}
+
+func (repository *LocationRepository) UpdateZone(ID *string, zone *Zone) error {
+	return repository.ZoneRepository.Update(ID, zone)
 }
