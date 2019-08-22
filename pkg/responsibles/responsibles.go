bccd773097de677aa87eff06263e08e484d6c5c2
Alejandro Rivera <seeealejandro@gmail.com>
2019-08-22 17:26:26 -0500

    [Progress] Responsibles module added
    - Users model and repository

--- pkg/responsibles/responsibles.go
+++ pkg/responsibles/responsibles.go
@@ -5,12 +5,13 @@ type User struct {
 	Name  *string
 	Email *string
 	// Those are the category where this guy is an EXPERT
-	CategoriesID []*string
+	ProductIDs []*string
 }
 
 type UserRepository interface {
 	Find(ID *string) (*User, error)
 	FindMany(ids []*string) ([]*User, error)
+	FindByProductIDs(ids []*string) ([]*User, error)
 	Store(*User) error
 	Remove(ID *string) error
 }
